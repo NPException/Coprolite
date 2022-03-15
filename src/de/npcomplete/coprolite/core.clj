4647072eb727c0174451fb282d2da08cfecce921
NPException <npexception42@gmail.com>
2022-03-15 20:01:47 +0100

    create base components of DB

--- src/de/npcomplete/coprolite/core.clj
+++ src/de/npcomplete/coprolite/core.clj
@@ -1,7 +1,32 @@
 (ns de.npcomplete.coprolite.core
   (:gen-class))
 
+(defrecord Database [layers top-id curr-time])
+
+
+(defrecord Layer [storage VAET AVET VEAT EAVT])
+
+
+(defrecord Entity [id attributes])
+
+(defn make-entity
+  ([] (make-entity :db/no-id-yet))
+  ([id] (Entity. id {})))
+
+
+(defrecord Attribute [name value ts prev-ts])
+
+(defn make-attribute
+  [name value type
+   & {:keys [cardinality] :or {cardinality :db/single}}]
+  {:pre [(contains? #{:db/single :db/multiple} cardinality)]}
+  (with-meta (Attribute. name value -1 -1) {:type type :cardinality cardinality}))
+
+(defn add-attribute [entity attribute]
+  (let [attr-id (keyword (:name attribute))]
+    (assoc-in entity [:attributes attr-id] attribute)))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+  [& _args]
   (println "Hello, World!"))
