fd4666b566134668393ab7fc53a9332f2d3ab601
NPException <npexception42@gmail.com>
2022-03-29 20:39:07 +0200

    continue query functionality

--- src/de/npcomplete/coprolite/query.clj
+++ src/de/npcomplete/coprolite/query.clj
@@ -1,5 +1,6 @@
 (ns de.npcomplete.coprolite.query
-  (:require [clojure.string :as str]
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
             [de.npcomplete.coprolite.core :as core]
             [de.npcomplete.coprolite.util :as u]))
 
@@ -14,7 +15,7 @@
         (or (and accept-underscore? (= x '_))
             (str/starts-with? (name x) "?")))))
 
-(defmacro clause-term-expr
+(defn clause-term-expr
   [clause-term]
   (cond
     (variable? clause-term)                                 ; variable
@@ -31,7 +32,7 @@
     :error
     (throw (ex-info "Term not matched by any condition" {:clause-term clause-term}))))
 
-(defmacro clause-term-variable
+(defn ^:private clause-term-variable-fn
   "Returns the (first) variable of the given term"
   [clause-term]
   (->> (u/collify clause-term)
@@ -41,9 +42,9 @@
 (defmacro pred-clause
   "Build a predicate clause from a 3 element query EAV clause."
   [clause]
-  (with-meta
-    `[~@(map (fn [x] `(clause-term-expr ~x)) clause)]
-    {:db/variable `[~@(map (fn [x] `(clause-term-variable ~x)) clause)]}))
+  `(with-meta
+     [~@(map clause-term-expr clause)]
+     {:db/variable (quote [~@(map clause-term-variable-fn clause)])}))
 
 (defmacro q-clauses-to-pred-clauses
   [clauses]
@@ -63,25 +64,62 @@
 
 ;; query planning
 
+(defn filter-index
+  [index predicate-clauses]
+  (for [[e a v :as pred-clause] predicate-clauses
+        :let [[lvl1-pred lvl2-pred lvl3-pred] ((core/from-eav index) e a v)]
+        ; keys and values of the first level
+        [k1 l2map] index
+        :when (try (lvl1-pred k1) (catch Exception _ false))
+        ; keys and values of the second level
+        [k2 l3-set] l2map
+        :when (try (lvl2-pred k2) (catch Exception _ false))
+        :let [res (into #{} (filter lvl3-pred) l3-set)]]
+    (with-meta [k1 k2 res] (meta pred-clause))))
+
+(defn items-that-answer-all-conditions
+  [items-seq ^long num-of-conditions]
+  (->> items-seq                                            ; take the sets of items
+       (reduce into [])                                     ; reduce all the sets into one vector
+       (frequencies)                                        ; count for each item in how many sets it was in
+       (into #{}
+         (keep (fn [[item ^long n]]
+                 (when (<= num-of-conditions n)             ; items that answered all conditions
+                   item))))))
+
+(defn mask-path-leaf-with-items
+  [relevant-items path]
+  (update-in path [2] set/intersection relevant-items))
+
+(defn query-index
+  [index pred-clauses]
+  (let [result-clauses         (filter-index index pred-clauses)
+        relevant-items         (items-that-answer-all-conditions (map peek result-clauses) (count pred-clauses))
+        cleaned-result-clauses (map #(mask-path-leaf-with-items relevant-items %) result-clauses)]
+    (filter #(seq (peek %)) cleaned-result-clauses)))
+
+;; NOTE: continue at "Table 10.7"
+
 (defn single-index-query-plan
-  [query indx db]
-  (let [q-res (query-index (core/index-at db indx) query)]
-    (bind-variables-to-query q-res (core/index-at db indx))))
+  [query-pred-clauses index-kw db]
+  (let [db-index (core/index-at db index-kw)
+        q-res (query-index db-index query-pred-clauses)]
+    (bind-variables-to-query q-res db-index)))
 
 (defn ^:private collapse-variable-vectors
   [accV v]
   (mapv #(when (= %1 %2) %1) accV v))
 
 (defn index-of-joining-variable
-  ^long [query-clauses]
-  (->> query-clauses
+  ^long [query-pred-clauses]
+  (->> query-pred-clauses
        (mapv #(:db/variable (meta %)))
        (reduce collapse-variable-vectors)
        (keep-indexed #(when (variable? %2 false) %1))
        first))
 
 (defn build-query-plan
-  [query]
-  (let [join-index (index-of-joining-variable query)
-        db-index   (case join-index 0 :AVET 1 :VEAT 2 :EAVT)]
-    (partial single-index-query-plan query db-index)))
+  [query-pred-clauses]
+  (let [join-index  (index-of-joining-variable query-pred-clauses)
+        db-index-kw (case join-index 0 :AVET 1 :VEAT 2 :EAVT)]
+    #(single-index-query-plan query-pred-clauses db-index-kw %)))
